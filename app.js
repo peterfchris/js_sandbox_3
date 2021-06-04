ac675714018cde160469cce0bedb409053e10d0b
PeterChristensen <peter.f.chris@gmail.com>
2021-06-04 08:40:13 -0600

    regular expressions notes - pt 1

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const user = { email: 'jdoe@gmail.com'}
+let re
+re = /hello/
+re = /hello/i //i - case sensitive
+// re = /hello/g //g - global search
 
-try {
-    // Produce a ReferenceError
-    // myFunction()
+// console.log(re)
+// console.log(re.source)
 
-    // TypeError
-    // null.myFunction()
+// exec() - return result in an array or null
+// const result = re.exec('hello world')
 
-    // SyntaxError
-    // eval ('Hello World')
+// console.log(result)
+// console.log(result[0])
+// console.log(result.index)
+// console.log(result.input)
 
-    // URIError
-    // decodeURIComponent('%')
+// test() - returns true or false
+// const result = re.test('Hello')
+// console.log(result)
 
-    if(!user.name) {
-        throw 'User has no name'
-    }
+// match() - return result array or null (like exec, but a little different)
 
-} catch(e){
-    console.log(e)
-    // console.log(e.message)
-    // console.log(e.name)
-    // console.log(e instanceof TypeError)
-} finally {
-    console.log('Finally runs regardless of result...')
-}
+// const str = 'Hello There'
+// const result = str.match(re)
+// console.log(result)
 
-console.log('Program continues...')
+// Search() - returns index of the first match if not found returns -1
+// const str = 'Brad hello there'
+// const result = str.search(re)
+// console.log(result)
+
+// replace() - return new string with some or all matches of a pattern
+const str = 'Hello there'
+const newStr = str.replace(re, 'Hi')
+console.log(newStr)
