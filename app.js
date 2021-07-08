c8518828eb95c27d4399b473fe04ab3774923a57
PeterChristensen <peter.f.chris@gmail.com>
2021-07-08 15:45:31 -0600

    reg exps + metacharacter symbols

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let re
-re = /hello/
-re = /hello/i //i - case sensitive
+// let re
+// re = /hello/
+// re = /hello/i //i - case sensitive
 // re = /hello/g //g - global search
 
 // console.log(re)
@@ -30,6 +30,38 @@ re = /hello/i //i - case sensitive
 // console.log(result)
 
 // replace() - return new string with some or all matches of a pattern
-const str = 'Hello there'
-const newStr = str.replace(re, 'Hi')
-console.log(newStr)
+// const str = 'Hello there'
+// const newStr = str.replace(re, 'Hi')
+// console.log(newStr)
+
+let re
+// literal characters
+re = /hello/
+re = /hello/i
+
+// metacharacter symbols
+re = /^h/i  //must start with
+re = /d$/i  //must end with
+re = /^hello$/i  //begins and ends with
+re = /he.lo/i  //matches any ONE character
+re = /he*lo/i  //matches any character 0 or more times
+re = /gre?a?y/i //optional character
+re = /gre?a?y\?/i //escape character
+
+
+// String to match
+const str = 'gray?'
+
+// log results
+const result = re.exec(str)
+console.log(result)
+
+function reTest(re, str) {
+    if(re.test(str)) {
+        console.log(`${str} matches ${re.source}`)
+    } else {
+        console.log(`${str} does NOT match ${re.source}`)
+    }
+}
+
+reTest(re, str)
